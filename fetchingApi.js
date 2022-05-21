bbf70269fb1a758b96ec013dfc7faa7566f00bd3
Your Name <luis.martinez33@protonmail.com>
2022-05-21 15:42:18 -0500

    test

--- fetchingApi.js
+++ fetchingApi.js
@@ -1,6 +1,15 @@
-API_KEY = "YTk1ZTZhOTctNjhmYy00NDI5LWE5YzItZDFiNTM0OTU1NDg5"
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+        'X-RapidAPI-Key': '1673859b58msh22ca0430778cb65p1c244fjsn5e68ca46937d'
+    }
+};
 
-fetch(`https://api.napster.com/v2.1/tracks/top?apikey=${API_KEY}`)
-    .then((response) => {
-        console.log(response.body)
-    });
+fetch('https://deezerdevs-deezer.p.rapidapi.com/album/302127', options)
+    .then(response => response.json())
+    .then(response => console.log(response.tracks.data))
+    .catch(err => console.error(err));
+
+const testClass = document.getElementsByClassName("test");
+
