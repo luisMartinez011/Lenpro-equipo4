d25a9ff3e91bbd0de0b6ccbccf422e2596721f1f
Your Name <luis.martinez33@protonmail.com>
2022-05-21 22:22:05 -0500

    Display de canciones terminada

--- fetchingApi.js
+++ fetchingApi.js
@@ -1,15 +1,34 @@
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
         'X-RapidAPI-Key': '1673859b58msh22ca0430778cb65p1c244fjsn5e68ca46937d'
     }
 };
 
-fetch('https://deezerdevs-deezer.p.rapidapi.com/album/302127', options)
+const URL = 'https://spotify23.p.rapidapi.com/playlist_tracks/?id=4QR0vsbUW1pI6BdoZAiolQ&offset=0&limit=18'
+
+fetch(URL, options)
     .then(response => response.json())
-    .then(response => console.log(response.tracks.data))
+    .then(response => response.items)
+    .then(response => response.map(song => {
+
+        const div = document.createElement("div");
+        const audio = document.createElement("audio");
+        const source = document.createElement("source");
+        const albums = document.getElementById("albums");
+
+        source.src = song.track.preview_url;
+        source.type = "audio/mpeg";
+        audio.appendChild(source);
+
+        div.style.backgroundImage = `url(${song.track.album.images[0].url})`
+        audio.controls = true
+
+        div.appendChild(audio)
+        //img.src = song.track.album.images[0].url;
+        //source.type = "audio/mpeg"
+        albums.appendChild(div);
+
+    }))
     .catch(err => console.error(err));
-
-const testClass = document.getElementsByClassName("test");
-
